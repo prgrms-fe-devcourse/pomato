c2e4c19297468a5979a89c79961b3eb1612719a6
elecharu <elec_haru@naver.com>
2025-10-03 16:24:19 +0900

    feat: 폰트 적용 및 디자인 토큰 생성

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
 
+import "./styles/global.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
