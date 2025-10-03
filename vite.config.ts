cfdf2fb337bf69c7186307871a7faf584e86d74f
SeHyun <31209436+sehyun0518@users.noreply.github.com>
2025-10-03 15:17:37 +0900

    chore: folder architecture (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,20 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@data": path.resolve(__dirname, "src/data"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@layout": path.resolve(__dirname, "src/layout"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@tests": path.resolve(__dirname, "src/tests"),
+      "@type": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
 });
