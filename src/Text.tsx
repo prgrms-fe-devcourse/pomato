497d8ed96886b518016a1f1d5de231b4867b99da
elecharu <elec_haru@naver.com>
2025-10-03 14:44:37 +0900

    feat: 디자인 토큰 설정

---
+++ src/Text.tsx
@@ -0,0 +1,33 @@
+export default function text() {
+  return (
+    <>
+      {/* Label 사용 예시 */}
+      <p className="paragraph-text-l">paragraph(Large)</p>
+      <p className="paragraph-text-m">paragraph(Medium)</p>
+      <p className="paragraph-text-s">paragraph(Small)</p>
+      <br />
+      {/* Label 사용 예시 */}
+      <p className="lable-text-l">Large Label (Bold)</p>
+      <p className="lable-text-l-semibold ">Large Label (SemiBold)</p>
+      <p className="lable-text-l-bold">Large Label (Medium)</p>
+      <br />
+      <p className="lable-text-m">Large Label (Bold)</p>
+      <p className="lable-text-m-semibold ">Large Label (SemiBold)</p>
+      <p className="lable-text-m-bold">Large Label (Medium)</p>
+      <br />
+      <p className="lable-text-s">Large Label (Bold)</p>
+      <p className="lable-text-s-semibold ">Large Label (SemiBold)</p>
+      <p className="lable-text-s-bold">Large Label (Medium)</p>
+      <br />
+      <p className="lable-text-xs">Large Label (Bold)</p>
+      <p className="lable-text-xs-semibold ">Large Label (SemiBold)</p>
+      <p className="lable-text-xs-bold">Large Label (Medium)</p>
+      <br />
+      {/* Heading 사용 예시 */}
+      <h1 className="heading-text-l">Large Heading</h1>
+      <h2 className="heading-text-m">Medium Heading</h2>
+      <h3 className="heading-text-s">Small Heading</h3>
+      <h4 className="heading-text-xs">XSmall Heading</h4>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import Text from "./Text.tsx";
+
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    {/* 실제 페이지  */}
     <App />
+    {/* 디자인 토큰 확인 페이지 */}
+    <Text />
   </StrictMode>,
 );
