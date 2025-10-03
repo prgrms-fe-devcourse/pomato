412ae9ea247075af39c5b2ec7184dcd139843324
elecharu <elec_haru@naver.com>
2025-10-03 14:44:37 +0900

    feat: 디자인 토큰 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import App from "./App.tsx";
+import Text from "./Text.tsx";
+
+import "./index.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    {/* 실제 페이지  */}
     <App />
+    {/* 디자인 토큰 확인 페이지 */}
+    <Text />
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"
 import path from "node:path";
 
 export default defineConfig({
@@ -7,6 +8,20 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+      // 폴더 구조가 잡히면 수정 필요
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+      "@icons": path.resolve(__dirname, "./src/assets/icons"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+      "@fonts": path.resolve(__dirname, "./src/assets/fonts"),
     },
   },
 });
