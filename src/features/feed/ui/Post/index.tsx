b8931008be273fe164bcac47d50fac59e8820a54
SeHyun <rla6170@naver.com>
2025-10-14 10:33:57 +0900

    feat: Feed Post 스타일링

---
+++ src/features/feed/ui/Post/index.tsx
@@ -0,0 +1,53 @@
+import { Ellipsis, Heart, MessageCircle } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
+export default function Post() {
+  return (
+    <div
+      className={twMerge(
+        "bg-wh/4 border-wh/6 dark:bg-bl/15 hover:bg-wh/10 hover:border-wh/12 hover:dark:bg-bl/30 hover:dark:border-bl/10 border-1",
+        "flex flex-col gap-[16px] rounded-[12px] px-[24px] py-[20px]",
+      )}
+    >
+      <div className="flex items-center justify-between">
+        <div className="flex gap-[12px]">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
+            alt="feed-post-avatar"
+            className="border-wh/15 h-[52px] w-[52px] rounded-full border-2 object-cover"
+          />
+
+          <div className="flex flex-col justify-start">
+            <span className="label-text-m-semibold text-wh">name</span>
+            <span className="label-text-s text-wh/60">id and times</span>
+          </div>
+        </div>
+        <button className="text-wh hover:bg-wh/15 flex h-[24px] w-[24px] cursor-pointer items-center justify-center rounded-[6px]">
+          <Ellipsis width={16} height={16} />
+        </button>
+      </div>
+      <div className="paragraph-text-m text-wh flex flex-col gap-[12px]">
+        <p className="paragraph-text-medium">
+          Just finished working on this amazing glassmorphism UI kit! 🎨✨
+        </p>
+        <div className="max-w-full overflow-hidden">
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
+            alt="example"
+            className="border-wh/10 h-auto max-w-full rounded-[12px] border-1 object-contain"
+          />
+        </div>
+      </div>
+      <div className="flex gap-[16px]">
+        <button className="text-wh/65 flex h-[34px] cursor-pointer items-center justify-center gap-[8px] rounded-[20px] px-[12px] hover:bg-pink-500/10 hover:text-pink-500">
+          <Heart width={15} height={15} />
+          <span className="label-text-xs">999+</span>
+        </button>
+        <button className="text-wh/65 hover:bg-wh/10 hover:text-wh/85 hover:dark:bg-bl/30 flex h-[34px] cursor-pointer items-center justify-center gap-[8px] rounded-[20px] px-[12px]">
+          <MessageCircle width={15} height={15} />
+          <span className="label-text-xs">999</span>
+        </button>
+      </div>
+    </div>
+  );
+}
