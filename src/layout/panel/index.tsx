b4560cc9bde8510d5dae5c3ac68f39c523020db2
SeHyun <rla6170@naver.com>
2025-10-14 14:41:20 +0900

    fix: panel open close 순서 변경

--- src/layout/panel/index.tsx
+++ src/layout/panel/index.tsx
@@ -10,20 +10,20 @@ export default function Panel() {
   return (
     <div>
       {open ? (
+        <div className="dark:bg-bl/30 border-wh/8 min-h-full w-[576px] border-1">
+          <Header close={() => setIsOpen(false)} />
+          <Nav />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      ) : (
         <button
           className="dark:bg-bl/40 border-wh/18 mt-[16px] mr-[16px] flex h-[40px] w-[40px] items-center justify-center rounded-[10px] border-1"
           onClick={() => setIsOpen(true)}
         >
           <Menu className="text-wh" width={16} height={16} />
         </button>
-      ) : (
-        <div className="dark:bg-bl/30 border-wh/8 min-h-full w-[576px] border-1">
-          <Header close={close} />
-          <Nav />
-          <main>
-            <Outlet />
-          </main>
-        </div>
       )}
     </div>
   );
