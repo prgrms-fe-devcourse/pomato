1db82db35089e548bc9a21c0f3544b00eccb82dc
SeHyun <rla6170@naver.com>
2025-10-14 11:53:07 +0900

    feat: router 설정

---
+++ src/routes/routes.ts
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router";
+
+// Layout
+import Layout from "@layout/Layout";
+// pages
+import Chart from "@pages/Chart";
+import Feed from "@pages/Feed";
+import Login from "@pages/Login";
+import Mate from "@pages/Mate";
+import Notification from "@pages/Notification";
+import Setting from "@pages/Setting";
+import Signup from "@pages/Signup";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        path: "mate",
+        Component: Mate,
+      },
+      {
+        path: "feed",
+        Component: Feed,
+      },
+      {
+        path: "chart",
+        Component: Chart,
+      },
+      {
+        path: "notification",
+        Component: Notification,
+      },
+      {
+        path: "setting",
+        Component: Setting,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
+  },
+]);
