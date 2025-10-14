e97875622ac9986826e472ca604e9ba48428d08c
SeHyun <rla6170@naver.com>
2025-10-14 11:54:34 +0900

    feat: layout navigation 연결

--- src/layout/panel/index.tsx
+++ src/layout/panel/index.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { NavLink, Outlet } from "react-router";
 
 interface PanelProps {
   close: () => void;
@@ -8,12 +9,26 @@ export default function Panel({ close }: PanelProps) {
   return (
     <div className="dark:bg-bl/30 border-wh/8 min-h-full w-[576px] border-1">
       <header className="flex h-[60px] justify-end px-[8px]">
+        <NavLink to="/login">로그인</NavLink>
+        <NavLink to="/signup">회원가입</NavLink>
         <button onClick={close}>
           <X width={16} height={16} className="text-wh" />
         </button>
       </header>
-      <nav></nav>
-      <main></main>
+      <nav>
+        <NavLink to="/mate" end>
+          메이트
+        </NavLink>
+        <NavLink to="/feed" end>
+          피드
+        </NavLink>
+        <NavLink to="/chart">차트</NavLink>
+        <NavLink to="/notification">내 소식</NavLink>
+        <NavLink to="/setting">설정</NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
