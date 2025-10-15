9e6153ec61442598ba59e0d5b253ea673705a623
SeHyun <31209436+sehyun0518@users.noreply.github.com>
2025-10-15 11:21:08 +0900

    feat: panel container 디자인 변경 반영 및 aria-label 추가 (#26)

--- src/layout/panel/index.tsx
+++ src/layout/panel/index.tsx
@@ -2,15 +2,19 @@ import { Menu } from "lucide-react";
 import { useState } from "react";
 import { Outlet } from "react-router";
 
+import Button from "@components/Button";
 import Header from "@layout/header";
 import Nav from "@layout/nav";
 
 export default function Panel() {
   const [open, setIsOpen] = useState(false);
   return (
-    <div>
+    <>
       {open ? (
-        <div className="dark:bg-bl/30 border-wh/8 min-h-full w-[576px] border-1">
+        <div
+          className="bg-wh/18 dark:bg-bl/18 border-wh/12 dark:border-wh/8 min-h-full w-[40%] max-w-[650px] min-w-[576px] rounded-[8px] border-1 backdrop-blur-[12px]"
+          aria-label="panel"
+        >
           <Header onClose={() => setIsOpen(false)} />
           <Nav />
           <main>
@@ -18,13 +22,10 @@ export default function Panel() {
           </main>
         </div>
       ) : (
-        <button
-          className="dark:bg-bl/40 border-wh/18 mt-[16px] mr-[16px] flex h-[40px] w-[40px] items-center justify-center rounded-[10px] border-1"
-          onClick={() => setIsOpen(true)}
-        >
+        <Button composition={"iconOnly"} onClick={() => setIsOpen(true)} aria-label="panel button">
           <Menu className="text-wh" width={16} height={16} />
-        </button>
+        </Button>
       )}
-    </div>
+    </>
   );
 }
