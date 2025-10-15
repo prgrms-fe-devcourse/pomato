b1ebf14aa3ec5a41c62989e57dda111c4a17bee9
SeHyun <rla6170@naver.com>
2025-10-15 12:41:59 +0900

    feat: header button 컴포넌트 적용

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,18 +1,24 @@
 import { X } from "lucide-react";
 import { NavLink } from "react-router";
 
+import Button from "@components/Button";
+
 type HeaderProps = {
   onClose: () => void;
 };
 
 export default function Header({ onClose }: HeaderProps) {
   return (
-    <header className="flex h-[60px] justify-end px-[8px]">
-      <NavLink to="/login">로그인</NavLink>
-      <NavLink to="/signup">회원가입</NavLink>
-      <button onClick={onClose}>
+    <header className="flex h-[60px] items-center justify-end px-[16px]">
+      <NavLink to="/login">
+        <Button intent="subtle">로그인</Button>
+      </NavLink>
+      <NavLink to="/signup">
+        <Button>회원가입</Button>
+      </NavLink>
+      <Button intent="reveal" onClick={onClose} composition="iconOnly">
         <X width={16} height={16} className="text-wh" />
-      </button>
+      </Button>
     </header>
   );
 }
