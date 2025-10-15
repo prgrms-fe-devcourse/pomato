1512d9e05d46ba69a4ef732140cb8069b11546fb
SeHyun <rla6170@naver.com>
2025-10-15 14:41:34 +0900

    chore: cva prettier 설정 적용

--- src/components/Avatar/variants.ts
+++ src/components/Avatar/variants.ts
@@ -3,16 +3,16 @@ import { cva } from "class-variance-authority";
 export const DEFAULT_SIZE = "m";
 
 export const avatarVariants = cva(
-  "flex bg-wh/25 dark:bg-bl/40 justify-center items-center border-2 border-wh/15 relative [&_label]:absolute [&_label]:border-wh [&_label]:rounded-full",
+  "bg-wh/25 dark:bg-bl/40 border-wh/15 [&_label]:border-wh relative flex items-center justify-center border-2 [&_label]:absolute [&_label]:rounded-full",
   {
     variants: {
       size: {
-        xxs: "w-[24px] h-[24px] rounded-[12px] [&_label]:w-[8px] [&_label]:h-[8px] [&_label]:border-1 [&_label]:left-[12px] [&_label]:top-[12px] [&>svg]:w-[12px] [&>svg]:h-[12px]",
-        xs: "w-[32px] h-[32px] rounded-[16px] [&_label]:w-[12px] [&_label]:h-[12px] [&_label]:border-2 [&_label]:left-[18px] [&_label]:top-[18px] [&>svg]:w-[16px] [&>svg]:h-[16px]",
-        s: "w-[40px] h-[40px] rounded-[20px] [&_label]:w-[14px] [&_label]:h-[14px] [&_label]:border-2 [&_label]:left-[24px] [&_label]:top-[24px] [&>svg]:w-[20px] [&>svg]:h-[20px]",
-        m: "w-[52px] h-[52px] rounded-[26px] [&_label]:w-[16px] [&_label]:h-[16px] [&_label]:border-2 [&_label]:left-[30px] [&_label]:top-[30px] [&>svg]:w-[26px] [&>svg]:h-[26px]",
-        l: "w-[80px] h-[80px] rounded-[40px] [&_label]:w-[20px] [&_label]:h-[20px] [&_label]:border-2 [&_label]:left-[52px] [&_label]:top-[52px] [&>svg]:w-[40px] [&>svg]:h-[40px]",
-        xl: "w-[120px] h-[120px] rounded-[60px] [&_label]:w-[24px] [&_label]:h-[24px] [&_label]:border-3 [&_label]:left-[88px] [&_label]:top-[88px] [&>svg]:w-[60px] [&>svg]:h-[60px]",
+        xxs: "h-[24px] w-[24px] rounded-[12px] [&_label]:top-[12px] [&_label]:left-[12px] [&_label]:h-[8px] [&_label]:w-[8px] [&_label]:border-1 [&>svg]:h-[12px] [&>svg]:w-[12px]",
+        xs: "h-[32px] w-[32px] rounded-[16px] [&_label]:top-[18px] [&_label]:left-[18px] [&_label]:h-[12px] [&_label]:w-[12px] [&_label]:border-2 [&>svg]:h-[16px] [&>svg]:w-[16px]",
+        s: "h-[40px] w-[40px] rounded-[20px] [&_label]:top-[24px] [&_label]:left-[24px] [&_label]:h-[14px] [&_label]:w-[14px] [&_label]:border-2 [&>svg]:h-[20px] [&>svg]:w-[20px]",
+        m: "h-[52px] w-[52px] rounded-[26px] [&_label]:top-[30px] [&_label]:left-[30px] [&_label]:h-[16px] [&_label]:w-[16px] [&_label]:border-2 [&>svg]:h-[26px] [&>svg]:w-[26px]",
+        l: "h-[80px] w-[80px] rounded-[40px] [&_label]:top-[52px] [&_label]:left-[52px] [&_label]:h-[20px] [&_label]:w-[20px] [&_label]:border-2 [&>svg]:h-[40px] [&>svg]:w-[40px]",
+        xl: "h-[120px] w-[120px] rounded-[60px] [&_label]:top-[88px] [&_label]:left-[88px] [&_label]:h-[24px] [&_label]:w-[24px] [&_label]:border-3 [&>svg]:h-[60px] [&>svg]:w-[60px]",
       },
     },
     defaultVariants: {
@@ -22,16 +22,16 @@ export const avatarVariants = cva(
 );
 
 export const editVariants = cva(
-  "absolute rounded-full border-wh bg-wh/90 dark:bg-bl/90 text-bl dark:text-wh flex items-center justify-center",
+  "border-wh bg-wh/90 dark:bg-bl/90 text-bl dark:text-wh absolute flex items-center justify-center rounded-full",
   {
     variants: {
       size: {
-        xxs: "!w-[12px] !h-[12px] !top-[8px] !left-[8px] border-1 [&>svg]:w-[4px] [&>button>svg]:h-[4px]",
-        xs: "!w-[16px] !h-[16px] !top-[12px] !left-[12px] border-2 [&>svg]:w-[6px] [&>button>svg]:h-[6px]",
-        s: "!w-[20px] !h-[20px] !top-[16px] !left-[16px] border-2 [&>svg]:w-[10px] [&>button>svg]:h-[10px]",
-        m: "!w-[24px] !h-[24px] !top-[26px] !left-[26px] border-2 [&>svg]:w-[14px] [&>button>svg]:h-[14px]",
-        l: "!w-[32px] !h-[32px] !top-[42px] !left-[42px] border-2 [&>svg]:w-[18px] [&>button>svg]:h-[18px]",
-        xl: "!w-[36px] !h-[36px] !top-[76px] !left-[76px] border-3 [&>svg]:w-[22px] [&>button>svg]:h-[22px]",
+        xxs: "!top-[8px] !left-[8px] !h-[12px] !w-[12px] border-1 [&>button>svg]:h-[4px] [&>svg]:w-[4px]",
+        xs: "!top-[12px] !left-[12px] !h-[16px] !w-[16px] border-2 [&>button>svg]:h-[6px] [&>svg]:w-[6px]",
+        s: "!top-[16px] !left-[16px] !h-[20px] !w-[20px] border-2 [&>button>svg]:h-[10px] [&>svg]:w-[10px]",
+        m: "!top-[26px] !left-[26px] !h-[24px] !w-[24px] border-2 [&>button>svg]:h-[14px] [&>svg]:w-[14px]",
+        l: "!top-[42px] !left-[42px] !h-[32px] !w-[32px] border-2 [&>button>svg]:h-[18px] [&>svg]:w-[18px]",
+        xl: "!top-[76px] !left-[76px] !h-[36px] !w-[36px] border-3 [&>button>svg]:h-[22px] [&>svg]:w-[22px]",
       },
     },
     defaultVariants: {
