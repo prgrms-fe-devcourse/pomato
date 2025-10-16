d87088e50c2ca75ec8b7aa4748910e2a40c4076c
Sage <106433812+sageherb@users.noreply.github.com>
2025-10-16 16:59:21 +0900

    refactor: 레이아웃 태그 구조 수정 (#50)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import Timer from "@features/timer";
-import Overlay from "@layout/overlay";
 import Panel from "@layout/panel";
 
 function App() {
   return (
-    <main className="min-h-dvh w-full">
-      <Overlay>
-        <Timer />
-        <Panel />
-      </Overlay>
+    <main className="flex min-h-dvh w-full items-stretch bg-[radial-gradient(106.3%_106.3%_at_20%_30%,rgba(168,85,247,0.25)_0%,rgba(0,0,0,0)_40%),radial-gradient(113.14%_113.14%_at_80%_20%,rgba(236,72,153,0.20)_0%,rgba(0,0,0,0)_40%),radial-gradient(100%_100%_at_40%_80%,rgba(59,130,246,0.20)_0%,rgba(0,0,0,0)_40%),radial-gradient(70.71%_70.71%_at_50%_50%,rgba(23,23,27,0.50)_0%,rgba(23,23,27,0.75)_100%)] bg-cover bg-no-repeat pt-[16px] pr-[16px] pb-[16px]">
+      <Timer />
+      <Panel />
     </main>
   );
 }
