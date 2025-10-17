180f1630a19829aac75553b2b6e6d4dab631e1fe
elecharu <elec_haru@naver.com>
2025-10-17 17:17:32 +0900

    fix: 스크롤 바 전역 적용 및 feed 페이지 list 영역 스크롤 기능 추가

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -6,7 +6,7 @@ import PostList from "@features/feed/ui/PostList";
 
 export default function Feed() {
   return (
-    <div className="flex flex-col gap-[12px] p-[16px] select-none">
+    <div className="flex h-screen flex-col gap-[12px] p-[16px] select-none">
       {/* 글쓰기 영역 + 구분선*/}
       <FeedHeader />
 
@@ -14,7 +14,9 @@ export default function Feed() {
       <Input Icon={{ Component: Search, align: "right" }} placeholder="검색어를 입력하세요..." />
 
       {/* Post List 영역 */}
-      <PostList />
+      <div className="pc-scroll min-h-0 flex-1 overflow-y-auto">
+        <PostList />
+      </div>
     </div>
   );
 }
