890858395d5245875f863d17e8b1f489a5fabf57
SeHyun <rla6170@naver.com>
2025-10-19 23:40:35 +0900

    feat: room supabase api 추가

---
+++ src/features/dm/api/room.ts
@@ -0,0 +1,40 @@
+import type { Database, Tables } from "@type/supabase";
+import supabase from "@utils/supabase";
+
+type DmConversationFunction = Database["public"]["Functions"]["get_or_create_dm_conversation"];
+
+export const getChatRoomIdByUser = async ({
+  _user_a: userAId,
+  _user_b: userBId,
+}: DmConversationFunction["Args"]): Promise<DmConversationFunction["Returns"]> => {
+  const { data, error } = await supabase.rpc<
+    "get_or_create_dm_conversation",
+    DmConversationFunction
+  >("get_or_create_dm_conversation", {
+    _user_a: userAId,
+    _user_b: userBId,
+  });
+
+  if (error) throw error;
+
+  return data;
+};
+
+type DmConversationTable = Tables<"dm_conversations">;
+
+export const getMyChatRoomIds = async (): Promise<DmConversationTable[]> => {
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+
+  if (userError) throw userError;
+
+  const { data, error: dataError } = await supabase
+    .from("dm_conversations")
+    .select<"*", DmConversationTable>("*")
+    .or(`user_a.eq.${user?.id},user.b.eq.${user?.id}`);
+
+  if (dataError) throw dataError;
+  return data;
+};
