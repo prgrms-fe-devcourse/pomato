f4c530a5ed91a93c349a11497fb822b1a2da148a
SeHyun <rla6170@naver.com>
2025-10-20 10:17:20 +0900

    feat: loader props 전달

--- src/features/dm/ui/ChatBubble/index.tsx
+++ src/features/dm/ui/ChatBubble/index.tsx
@@ -3,15 +3,14 @@ import { twMerge } from "tailwind-merge";
 import { ChatBubbleVariants } from "@features/dm/ui/ChatBubble/variants";
 
 type ChatBubbleType = {
+  text: string;
   isMine?: boolean;
 };
 
-export default function ChatBubble({ isMine = true }: ChatBubbleType) {
+export default function ChatBubble({ isMine = true, text }: ChatBubbleType) {
   return (
     <div className={twMerge("flex flex-col gap-[6px]", isMine ? "items-end" : "items-start")}>
-      <div className={ChatBubbleVariants({ isMine })}>
-        Hey! How's the glassmorphism project coming along?
-      </div>
+      <div className={ChatBubbleVariants({ isMine })}>{text}</div>
       <span className="label-text-xs text-wh/60">2:34 PM</span>
     </div>
   );
--- src/features/dm/ui/ChatList/index.tsx
+++ src/features/dm/ui/ChatList/index.tsx
@@ -1,14 +1,23 @@
 import { useNavigate } from "react-router";
 
+import type { DmConversationTable } from "@features/dm/types/conversation.type";
 import ChatCard from "@features/dm/ui/ChatCard";
 
-export default function ChatList() {
+export type ChatListProps = {
+  rooms: DmConversationTable[];
+};
+
+export default function ChatList({ rooms = [] }: ChatListProps) {
   const navigate = useNavigate();
   return (
     <ul className="flex flex-col gap-[4px]">
-      <li onClick={() => void navigate("123")}>
-        <ChatCard name="Sera" message="message" />
-      </li>
+      {rooms.map((value) => {
+        return (
+          <li key={value.id} id={value.id} onClick={() => void navigate(`${value.id}`)}>
+            <ChatCard name="Sera" message="message" />
+          </li>
+        );
+      })}
     </ul>
   );
 }
