196c37a94886f74f999ddc181b1180f455ce5276
SeHyun <rla6170@naver.com>
2025-10-20 10:16:55 +0900

    feat: pages use loader data 추가

--- src/pages/dm/Chat.tsx
+++ src/pages/dm/Chat.tsx
@@ -1,12 +1,16 @@
+import { useLoaderData } from "react-router";
+
+import type { DmMessagesTable } from "@features/dm/types/messages.type";
 import ChatHeader from "@features/dm/ui/ChatHeader";
 import ChatInput from "@features/dm/ui/ChatInput";
 import ChatMessages from "@features/dm/ui/ChatMessages";
 
 export default function Chat() {
+  const messages = useLoaderData<DmMessagesTable["Row"][]>();
   return (
     <div className="flex flex-1 flex-col">
       <ChatHeader />
-      <ChatMessages />
+      <ChatMessages messages={messages} />
       <ChatInput />
     </div>
   );
