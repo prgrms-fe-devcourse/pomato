a85a835c8fa3be3de497a1ba8b0fd30d8ad04680
Sage <106433812+sageherb@users.noreply.github.com>
2025-10-20 09:30:37 +0900

    feat: 다크 모드 및 반응형 구현 (#62)

---
+++ src/layout/main/index.tsx
@@ -0,0 +1,27 @@
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import Button from "@components/Button";
+
+export default function Main({ children }: { children: React.ReactNode }) {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", isDark);
+  }, [isDark]);
+
+  return (
+    <main className="group bg-gradient relative flex min-h-dvh w-full items-stretch gap-4 p-4">
+      <Button
+        onClick={() => setIsDark((prev) => !prev)}
+        composition="iconOnly"
+        className="absolute top-4 left-4 group-has-[section[aria-label='Panel']]:max-[800px]:hidden"
+        aria-label="Theme Toggle Button"
+      >
+        <Sun className="hidden dark:block" />
+        <Moon className="block dark:hidden" />
+      </Button>
+      {children}
+    </main>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/global.css";
 import { RouterProvider } from "react-router";
 
 import { router } from "@routes/routes.ts";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
