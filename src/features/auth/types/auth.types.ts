06309742f8d435b6545aa8dc6a83ecbd42488f39
Sage <prestoferoce@gmail.com>
2025-10-20 16:48:56 +0900

    refactor: Profile, OAuthProvider 타입 수정

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -1,8 +1,9 @@
-import type { Database, Tables } from "@type/database.types";
+import type { Provider } from "@supabase/supabase-js";
 
-export type OAuthProviders = "google" | "github" | "kakao";
+import type { Tables } from "@type/database.types";
+
+export type OAuthProvider = Extract<Provider, "google" | "github" | "kakao">;
 
 export type ProfileRow = Tables<"profiles">;
 
-type DataProfile = Database["public"]["Tables"]["profiles"]["Row"];
-export type Profile = Pick<DataProfile, "user_id" | "display_name" | "username" | "avatar_url">;
+export type Profile = Pick<ProfileRow, "user_id" | "display_name" | "username" | "avatar_url">;
