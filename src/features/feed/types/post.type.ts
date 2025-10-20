00cdd1d43ce089b4fd5bc58fe6e1351d722112a3
elecharu <elec_haru@naver.com>
2025-10-20 12:34:21 +0900

    feat: supabase Database 타입 추가 선언

--- src/features/feed/types/post.type.ts
+++ src/features/feed/types/post.type.ts
@@ -1,3 +1,7 @@
 import type { Database } from "@type/database.types";
 
-export type PostTable = Database["public"]["Tables"]["posts"];
+export type PostTable = Database["public"]["Tables"]["posts"]; // POST 테이블 데이터 조회
+
+export type PostRow = Database["public"]["Tables"]["posts"]["Row"]; // SELECT 시
+export type PostInsert = Database["public"]["Tables"]["posts"]["Insert"]; // INSERT 시
+export type PostUpdate = Database["public"]["Tables"]["posts"]["Update"]; // UPDATE 시
