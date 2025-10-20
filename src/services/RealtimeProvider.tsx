8b6ab60f279a13a06976f6d8aa6a015c873a3658
SeHyun <rla6170@naver.com>
2025-10-20 17:48:03 +0900

    feat: start 1회 실행 dependency 제거

--- src/services/RealtimeProvider.tsx
+++ src/services/RealtimeProvider.tsx
@@ -14,7 +14,8 @@ export default function RealtimeProvider({ children }: RealtimeProviderProps) {
     return () => {
       realtimeCleanup();
     };
-  }, [start]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   usePresenceActiveUsers();
   // main에 위치한 함께하는 인원 컴포넌트를 위해 시작 이후 바로 채널 구독
   return <>{children}</>;
