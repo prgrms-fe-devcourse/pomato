2406db3bb17358323760d76a31b8b51e7d677498
SeHyun <rla6170@naver.com>
2025-10-20 10:15:02 +0900

    fix: api eq 수정

--- src/features/dm/api/room.ts
+++ src/features/dm/api/room.ts
@@ -33,12 +33,10 @@ export const getMyChatRoomIds = async (): Promise<GetMyChatRoomIdsReturn> => {
   } = await supabase.auth.getUser();
 
   if (userError) return { isLogin: false, rooms: [] };
-
   const { data, error: dataError } = await supabase
     .from("dm_conversations")
     .select<"*", DmConversationTable>("*")
-    .or(`user_a.eq.${user?.id},user.b.eq.${user?.id}`);
-
+    .or(`user_a.eq.${user?.id},user_b.eq.${user?.id}`);
   if (dataError) throw dataError;
   return { isLogin: true, rooms: data };
 };
