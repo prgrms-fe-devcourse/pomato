294272b08ee548ab26c94644a3e5f8b0a429b3e6
SeHyun <rla6170@naver.com>
2025-10-20 18:31:38 +0900

    feat: database.type 최신화 반영

--- src/features/dm/types/conversation.type.ts
+++ src/features/dm/types/conversation.type.ts
@@ -1,13 +1,6 @@
-import type { Tables } from "@type/database.types";
+import type { Tables, Database } from "@type/database.types";
 
 export type DmConversationTable = Tables<"dm_conversations">;
 
 export type DmConversationFunction =
-  //Database["public"]["Functions"]["get_or_create_dm_conversation"]
-  {
-    Args: {
-      _user_a: string;
-      _user_b: string;
-    };
-    Returns: string;
-  };
+  Database["public"]["Functions"]["get_or_create_dm_conversation"];
