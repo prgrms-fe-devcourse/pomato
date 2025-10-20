a506bb13acf2459badcb039caed3e27f21d733c8
Sage <prestoferoce@gmail.com>
2025-10-20 15:36:09 +0900

    refactor: getProfile 파일 이름 변경 및 에러 시 리턴 값 null로 변경

--- src/features/auth/api/profile.ts
+++ src/features/auth/api/profile.ts
@@ -2,10 +2,13 @@ import supabase from "@utils/supabase";
 
 import type { ProfileRow } from "../types/auth.types";
 
-export const getProfile = async (id: string): Promise<ProfileRow> => {
+export const getProfile = async (id: string): Promise<ProfileRow | null> => {
   const { data, error } = await supabase.from("profiles").select("*").eq("user_id", id).single();
 
-  if (error || !data) throw new Error("프로필 조회 오류");
+  if (error || !data) {
+    console.error("프로필 조회 오류:", error);
+    return null;
+  }
 
   return data;
 };
