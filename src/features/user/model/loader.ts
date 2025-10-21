b517f4521d0fc86d751f63787578e6f1e69a1b06
SeHyun <rla6170@naver.com>
2025-10-21 12:38:16 +0900

    fix: routes.ts conflict 수정

--- src/features/user/model/loader.ts
+++ src/features/user/model/loader.ts
@@ -1,6 +1,6 @@
 import { getAllUsersWithoutSelf } from "@features/user/api/user";
 
 export const mateLoader = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await getAllUsersWithoutSelf();
+  const users = await getAllUsersWithoutSelf();
+  return users;
 };
