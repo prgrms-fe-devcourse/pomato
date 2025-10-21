8a0fe2a46aae926a884c2147fc6b0d53b744053d
SeHyun <rla6170@naver.com>
2025-10-21 23:52:41 +0900

    feat: 개인 채팅방 정보 조회 및 연결용 loader 추가 및 연결

--- src/pages/dm/Chat.tsx
+++ src/pages/dm/Chat.tsx
@@ -1,15 +1,25 @@
 import { useLoaderData } from "react-router";
 
-import type { DmMessagesTable } from "@features/dm/types/messages.type";
+import { useUserId } from "@features/auth/model/useAuthStore";
+import type { MessageLoaderReturn } from "@features/dm/model/loader";
 import ChatHeader from "@features/dm/ui/ChatHeader";
 import ChatInput from "@features/dm/ui/ChatInput";
 import ChatMessages from "@features/dm/ui/ChatMessages";
+import { getOtherUser } from "@features/user/util/user";
 
 export default function Chat() {
-  const messages = useLoaderData<DmMessagesTable["Row"][]>();
+  const id = useUserId();
+  const { messages, roomInfo } = useLoaderData<MessageLoaderReturn>();
+
+  if (id === undefined) {
+    throw new Error("로그인을 확인할 수 없습니다.");
+  }
+  if (roomInfo === null) {
+    throw new Error("채팅방 정보를 가져올 수 없습니다");
+  }
   return (
     <div className="flex flex-1 flex-col">
-      <ChatHeader />
+      <ChatHeader partnerId={getOtherUser(id, roomInfo.user_a, roomInfo.user_b)} />
       <ChatMessages messages={messages} />
       <ChatInput />
     </div>
