64258bc5e36522b100cfa802de8fc9562a6844c2
Sage <prestoferoce@gmail.com>
2025-10-21 14:56:45 +0900

    feat: 타이머 음악 플레이어 UI로 변경 및 cva  파일 분리

---
+++ src/features/timer/variants.ts
@@ -0,0 +1,17 @@
+import { cva } from "class-variance-authority";
+
+export const dot = cva(
+  "focus-visible:ring-wh/12 h-2 w-2 rounded-full border outline-none focus-visible:ring-2",
+  {
+    variants: {
+      status: {
+        default: ["bg-wh/10 border-wh/20", "dark:bg-bl/20 dark:border-wh/8"],
+        active: ["bg-wh/30 border-wh/30", "dark:bg-bl/40 dark:border-wh/18 w-7.5"],
+        completed: ["bg-wh/50 border-wh/40", "dark:bg-bl/60 dark:border-wh/25"],
+      },
+    },
+    defaultVariants: {
+      status: "default",
+    },
+  },
+);
