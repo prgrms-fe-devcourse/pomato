978f8069ac4fda23a3d5a79ab913243db32a2fc7
SeHyun <rla6170@naver.com>
2025-10-21 17:06:52 +0900

    feat: postgre_change realtime channel 추가 및 구독

---
+++ src/features/user/hook/usePostgreActiveUser.ts
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+
+import { useActiveUsersStore } from "@features/user/store/useActiveUserStore";
+import { PostgreActiveUserTopic } from "@features/user/type/activeUsers.type";
+import { useRealtimeHandler } from "@hooks/useRealtimeHandler";
+import supabase from "@utils/supabase";
+
+export const usePosgreActiveUser = () => {
+  const { addChannel, started } = useRealtimeHandler();
+  const { setActiveUsers } = useActiveUsersStore();
+
+  useEffect(() => {
+    if (!started) return () => {};
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("active_users").select("id");
+      if (!error && data) {
+        const users = data.map((row) => ({ id: row.id }));
+        setActiveUsers(users);
+      }
+    };
+    const removeChannel = addChannel(
+      (supabaseClient) => {
+        const channel = supabaseClient.channel(PostgreActiveUserTopic);
+        channel.on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "active_users",
+          },
+          () => {
+            void fetchData();
+          },
+        );
+        return channel;
+      },
+      false,
+      {
+        onSubscribe: () => {
+          void fetchData();
+        },
+      },
+    );
+
+    return () => {
+      removeChannel();
+    };
+  }, [started, addChannel, setActiveUsers]);
+};
