70bfd38ff5882594c7862f08820d9dd856510af6
SeHyun <rla6170@naver.com>
2025-10-21 23:56:33 +0900

    feat: 개별 profile 연결 active user 판별 추가

--- src/features/dm/ui/ChatHeader/index.tsx
+++ src/features/dm/ui/ChatHeader/index.tsx
@@ -1,20 +1,43 @@
 import { ChevronLeft, MoreVertical } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import Avatar from "@components/Avatar";
+import { getProfile } from "@features/auth/api/profile";
+import { useActiveUsersStore } from "@features/user/store/useActiveUserStore";
+import type { ProfilesTable } from "@features/user/types/user.type";
 
-export default function ChatHeader() {
+type ChatHeaderType = {
+  partnerId: string;
+};
+
+export default function ChatHeader({ partnerId }: ChatHeaderType) {
   const navigate = useNavigate();
+  const [profile, setProfile] = useState<ProfilesTable["Row"]>();
+  const activeUsers = useActiveUsersStore((state) => state.activeUsers);
+
+  useEffect(() => {
+    const fetchPartner = async () => {
+      const data = await getProfile(partnerId);
+      if (data === null) throw new Error("사용자를 찾을 수 없습니다");
+      setProfile(data);
+    };
+
+    void fetchPartner();
+  }, [setProfile, partnerId]);
+
   return (
     <div className="border-wh/15 flex h-[64px] items-center justify-between border-b-1 px-[16px]">
       <div className="flex gap-[12px]">
         <button onClick={() => void navigate(-1)} className="cursor-pointer">
           <ChevronLeft width={24} height={24} />
         </button>
-        <Avatar size="s" />
+        <Avatar size="s" src={profile?.avatar_url ?? undefined} />
         <div className="flex flex-col justify-center">
-          <span className="label-text-medium-semibold">Jordan Taylor</span>
-          <span className="label-text-xs text-wh/75">오프라인</span>
+          <span className="label-text-medium-semibold">{profile?.display_name}</span>
+          <span className="label-text-xs text-wh/75">
+            {activeUsers.some((user) => user.id === partnerId) ? "Online" : "Offline"}
+          </span>
         </div>
       </div>
       <button>
--- src/features/dm/ui/ChatList/index.tsx
+++ src/features/dm/ui/ChatList/index.tsx
@@ -2,19 +2,21 @@ import { useNavigate } from "react-router";
 
 import type { DmConversationTable } from "@features/dm/types/conversation.type";
 import ChatCard from "@features/dm/ui/ChatCard";
+import { getOtherUser } from "@features/user/util/user";
 
 export type ChatListProps = {
   rooms: DmConversationTable[];
+  userId: string;
 };
 
-export default function ChatList({ rooms = [] }: ChatListProps) {
+export default function ChatList({ rooms = [], userId }: ChatListProps) {
   const navigate = useNavigate();
   return (
     <ul className="flex flex-col gap-[4px]">
       {rooms.map((room) => {
         return (
           <li key={room.id} id={room.id} onClick={() => void navigate(`${room.id}`)}>
-            <ChatCard name="Sera" message="message" />
+            <ChatCard message="message" userId={getOtherUser(userId, room.user_a, room.user_b)} />
           </li>
         );
       })}
