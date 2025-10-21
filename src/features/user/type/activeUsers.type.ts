61704780a689b7c74b734cf4e0191dc183d26ebd
SeHyun <rla6170@naver.com>
2025-10-21 17:04:16 +0900

    feat: postgre_change channel topic 추가

--- src/features/user/type/activeUsers.type.ts
+++ src/features/user/type/activeUsers.type.ts
@@ -1,1 +1,2 @@
-export const ActiveUsersTopic = "active_users" as const;
+export const PresenceActiveUsersTopic = "presence_active_users" as const;
+export const PostgreActiveUserTopic = "postgre_active_users" as const;
