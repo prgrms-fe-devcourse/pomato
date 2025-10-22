1b084fa517a1d2f81d493ff089b8af58d476afb8
Sage <prestoferoce@gmail.com>
2025-10-22 14:41:03 +0900

    feat: 프로그레스 퍼센트 구현

--- src/features/timer/ui/ProgressBar.tsx
+++ src/features/timer/ui/ProgressBar.tsx
@@ -7,13 +7,14 @@ export default function ProgressBar({
   focusMin: number;
   remainingSec: number;
 }) {
+  const progressPercent = (remainingSec / focusMin) * 100;
   return (
     <div className="flex items-center justify-center gap-2 text-xs">
       <span className="cursor-default tabular-nums">{toMMSS(remainingSec)}</span>
       <div className="bg-wh/15 dark:bg-bl/10 relative h-1 w-50 flex-1 rounded-full">
         <div
           className="bg-wh dark:bg-bl/45 absolute h-1 rounded-full"
-          style={{ width: "42%" }}
+          style={{ width: `${progressPercent}%` }}
         ></div>
       </div>
       <span className="cursor-default tabular-nums">{toMMSS(focusMin)}</span>
