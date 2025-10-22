e35bdc6047267d792aed178c917fb6e11c53dbd4
SeHyun <rla6170@naver.com>
2025-10-22 22:24:23 +0900

    fix: user id 위치 변경 반영

--- src/pages/dm/Dm.tsx
+++ src/pages/dm/Dm.tsx
@@ -1,14 +1,10 @@
-import { MessageCircle, Search } from "lucide-react";
-import { useLoaderData } from "react-router";
+import { MessageCircle } from "lucide-react";
 
 import EmptyState from "@components/Empty";
-import Input from "@components/Input";
-import type { GetMyChatRoomIdsReturn } from "@features/dm/api/room";
 import ChatList from "@features/dm/ui/ChatList";
 import { useUserId } from "@stores/useAuthStore";
 
 export default function Dm() {
-  const { rooms } = useLoaderData<GetMyChatRoomIdsReturn>();
   const id = useUserId();
   if (!id)
     return (
@@ -21,21 +17,10 @@ export default function Dm() {
         />
       </div>
     );
-  if (rooms.length === 0)
-    return (
-      <div className="flex flex-1 overflow-hidden p-[16px]">
-        <EmptyState
-          title="메세지가 없습니다"
-          description="아직 주고받은 메세지가 없습니다"
-          Icon={MessageCircle}
-          className="border-wh/6 rounded-[12px] border-1"
-        />
-      </div>
-    );
+
   return (
     <div className="flex flex-col gap-[12px] p-[16px]">
-      <Input Icon={{ Component: Search, align: "right" }} placeholder="검색어를 입력하세요..." />
-      <ChatList rooms={rooms} userId={id} />
+      <ChatList userId={id} rooms={[]} />
     </div>
   );
 }
