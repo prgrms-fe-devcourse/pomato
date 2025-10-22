75800c61bf45de1d2cca46bced11f527806c6b88
Sage <prestoferoce@gmail.com>
2025-10-22 10:44:31 +0900

    chore: database types 설정

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -5,6 +5,6 @@ import type { Database } from "@type/database.types";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+const supabase = createClient<Database, { PostgrestVersion: "13.0.5" }>(supabaseUrl, supabaseKey);
 
 export default supabase;
