055f123da6a4d4bc3ce1868db17c9d434bbefd39
SeHyun <rla6170@naver.com>
2025-10-23 11:39:38 +0900

    feat: 내소식 카드 리스트 컴포넌트

---
+++ src/features/Notification/ui/NotificationList/index.tsx
@@ -0,0 +1,35 @@
+import { Bell } from "lucide-react";
+
+import EmptyState from "@components/Empty";
+import { useNotificationStore } from "@features/notification/stores/useNotificationStore";
+import type { NotificationJsonbType } from "@features/notification/types/notification.type";
+import NotificationCard from "@features/notification/ui/NotificationCard";
+
+export default function NotificationList() {
+  const notifications = useNotificationStore((state) => state.notifications);
+
+  if (notifications.length === 0)
+    return (
+      <EmptyState
+        title="소식이 없습니다"
+        description="모든 소식을 확인했습니다"
+        className="border-wh/6 rounded-[12px] border-1"
+        Icon={Bell}
+      />
+    );
+
+  return (
+    <div>
+      {notifications.map((notification) => {
+        return (
+          <NotificationCard
+            key={notification.id}
+            type={notification.type}
+            payload={notification.payload as NotificationJsonbType}
+            notificationId={notification.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
