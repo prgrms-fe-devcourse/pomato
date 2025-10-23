824a7a97fbd4a9bc256b5dd159d80f2bb5fc2d98
SeHyun <rla6170@naver.com>
2025-10-23 11:36:42 +0900

    feat: 내소식 카드 전용 store

---
+++ src/features/Notification/stores/useNotificationStore.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+
+import type { NotificationTable } from "@features/notification/types/notification.type";
+
+type NotificationState = {
+  notifications: NotificationTable["Row"][];
+};
+
+type NotificationAction = {
+  setNotifications: (notifications: NotificationTable["Row"][]) => void;
+  removeNotifications: (notificationId: string) => void;
+  addNotification: (notification: NotificationTable["Row"]) => void;
+};
+
+export const useNotificationStore = create<NotificationState & NotificationAction>()((set) => ({
+  notifications: [],
+  setNotifications: (notifications) => set({ notifications: notifications }),
+  removeNotifications: (notificationId) =>
+    set((prev) => ({
+      notifications: prev.notifications.filter((n) => n.id !== notificationId),
+    })),
+  addNotification: (notification) =>
+    set((prev) => ({
+      notifications: prev.notifications.some((n) => n.id === notification.id)
+        ? prev.notifications
+        : [notification, ...prev.notifications],
+    })),
+}));
