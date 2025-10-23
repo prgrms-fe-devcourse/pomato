07a2b97a0fe7f28c20d070b6b5abfc7d939583ce
SeHyun <rla6170@naver.com>
2025-10-23 15:36:50 +0900

    feat: user input 분리

--- src/pages/Mate.tsx
+++ src/pages/Mate.tsx
@@ -1,20 +1,13 @@
-import { Search } from "lucide-react";
-
-import Input from "@components/Input";
-import { useUserStore } from "@features/user/store/useUserStore";
 import UserHeader from "@features/user/ui/UserHeader";
+import UserInput from "@features/user/ui/UserInput";
 import UserList from "@features/user/ui/UserList";
 
 export default function Mate() {
-  const users = useUserStore((state) => state.users);
   return (
-    <div className="flex flex-col gap-[12px] p-[16px]">
+    <div className="flex flex-1 flex-col gap-[12px] p-[16px]">
       <UserHeader />
-      <Input
-        Icon={{ Component: Search, align: "right" }}
-        placeholder="찾을 메이트의 이름을 검색해주세요..."
-      />
-      <UserList users={users} />
+      <UserInput />
+      <UserList />
     </div>
   );
 }
