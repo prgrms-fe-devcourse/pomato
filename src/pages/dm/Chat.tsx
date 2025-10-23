ad6cf1da0a44f9d341e60081308a31c7a28edbb5
SeHyun <rla6170@naver.com>
2025-10-23 14:31:34 +0900

    fix: 채팅방 loader 제거

--- src/pages/dm/Chat.tsx
+++ src/pages/dm/Chat.tsx
@@ -1,45 +1,32 @@
-import { useEffect } from "react";
-import { useLoaderData } from "react-router";
+import { useParams } from "react-router";
 
 import { useRoomChannel } from "@features/dm/hook/useRoomChannel";
-import type { MessageLoaderReturn } from "@features/dm/model/loader";
-import { useChatStore } from "@features/dm/store/useChatStore";
 import ChatHeader from "@features/dm/ui/ChatHeader";
 import ChatInput from "@features/dm/ui/ChatInput";
 import ChatMessages from "@features/dm/ui/ChatMessages";
-import { getOtherUser } from "@features/user/util/user";
 import { useUserId } from "@stores/useAuthStore";
 
 export default function Chat() {
   const id = useUserId();
-  const { messages, roomInfo } = useLoaderData<MessageLoaderReturn>();
+  const params = useParams();
 
-  if (id === undefined) {
+  if (!id || !params.id) {
     throw new Error("로그인을 확인할 수 없습니다.");
   }
-  if (roomInfo === null) {
-    throw new Error("채팅방 정보를 가져올 수 없습니다");
-  }
 
-  const addMessage = useChatStore((state) => state.addMessage);
-  const setMessages = useChatStore((state) => state.setMessages);
-  const { sendMessages } = useRoomChannel(roomInfo.id, addMessage);
-
-  useEffect(() => {
-    setMessages(messages); // 기존 채팅 내역 불러오기
-  }, [messages, setMessages]);
+  const { sendMessages } = useRoomChannel(params.id);
 
   const handleSendMessage = (message: string) => {
     void sendMessages({
       content: message,
-      conversation_id: roomInfo.id,
+      conversation_id: params.id as string,
       sender_id: id,
     });
   };
 
   return (
-    <div className="flex flex-1 flex-col">
-      <ChatHeader partnerId={getOtherUser(id, roomInfo.user_a, roomInfo.user_b)} />
+    <div className="pc-scroll min-h-0 flex-1 overflow-y-auto">
+      <ChatHeader conversationId={params.id} userId={id} />
       <ChatMessages />
       <ChatInput sendMessage={handleSendMessage} />
     </div>
